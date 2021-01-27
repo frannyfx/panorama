975a547a8bb8be611f06ae781adf31ce15b68526
Francesco Compagnoni <compfrancesco@gmail.com>
2021-01-27 07:31:13 +0000

    Add formal route to check authentication

--- src/server/web/router/routes/api/auth.ts
+++ src/server/web/router/routes/api/auth.ts
@@ -1,5 +1,5 @@
 /**
- * @file Routes handling requests to process repositories.
+ * @file Routes handling authentication.
  * @author Francesco Compagnoni
  */
 
@@ -16,24 +16,12 @@ const config : Config = loadConfig();
 import { Method } from "../../../../../shared/Method";
 import { Request, Route } from "../../Route";
 import { Codes, send } from "../../API";
-import queue from "../../../../processing/queue";
 
 let route : Array<Route> = [{
-	method: Method.POST,
-	url: "/api/repo", // TODO: Make RESTified.
+	method: Method.GET,
+	url: "/api/auth",
+	auth: true,
 	handler: async (request: Request, response: any) => {
-		// Add repo to queue to be processed.
-		let job = await queue.getRepoQueue()?.createJob({
-			repoId: 123
-		}).save();
-
-		// Add job event handlers
-		// ...
-		
-		// Generate ticket to listen to job events.
-		// sign(job.id | user.auth)
-
-		// Send OK with ticket.
 		send(response, Codes.OK);
 	}
 }];
