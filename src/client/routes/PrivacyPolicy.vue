f8bb79469ae60f79eaa9d8da0971d2192aee2983
Francesco Compagnoni <compfrancesco@gmail.com>
2021-03-25 19:30:12 +0000

    Improve privacy policy

--- src/client/routes/PrivacyPolicy.vue
+++ src/client/routes/PrivacyPolicy.vue
@@ -1,98 +1,100 @@
 <template>
-	<div class="page nav" :class="[theme]">
-		<div class="content container">
-			<div class="title">
-				<div class="eye"><font-awesome-icon icon="eye"/></div>
-				<button @click="switchTheme">
-					<font-awesome-icon icon="eye"/>
-					<span>Switch theme</span>
-				</button>
+	<div class="app" :class="[theme]">
+		<div class="page nav">
+			<div class="content container">
+				<div class="title">
+					<div class="eye"><font-awesome-icon icon="eye"/></div>
+					<button @click="switchTheme">
+						<font-awesome-icon icon="eye"/>
+						<span>Switch theme</span>
+					</button>
+				</div>
+				<h1>Privacy Policy</h1>
+				<i>Last updated March 25<sup>th</sup> 2021.</i>
+				<p>
+					<b>Panorama</b> ("Panorama", "we", or "us") is committed to protecting your privacy.
+					This Privacy policy explains the methods and reasons we collect, use, disclose, transfer and store your information.
+					If you have any questions about the contents of this policy, please contact us.
+				</p>
+				<h2>Information collected</h2>
+				<h3>Personal information</h3>
+				<p>
+					We collect and store your personal information when you choose to sign in to our service using GitHub.
+					The scope of the information collected is as follows:
+				</p>
+				<ul>
+					<li>Your GitHub account's unique identifier;</li>
+					<li>Your GitHub username;</li>
+					<li>The last time you used Panorama to analyse a repository;</li>
+					<li>Metadata about your GitHub profile picture.</li>
+				</ul>
+				<p>
+					It's important to note that the personal information of people who either own or have contributed to GitHub repositories that you have analysed using Panorama will also be collected in the same way.<br>
+					Additionally, during analysis, Panorama will temporarily process the email addresses and Git commit information of all repository contributors, though this data 
+					is never specifically stored or saved.
+				</p>
+				<h3>Information about repositories</h3>
+				<p>
+					Panorama will store metadata about the repositories that you have analysed using our service. 
+					This includes the following information:
+				</p>
+				<ul>
+					<li>The GitHub repository's unique identifier;</li>
+					<li>The name of the repository;</li>
+					<li>The last time the repository was analysed;</li>
+					<li>Analysis metadata and code analysis metrics tied to each contributor.</li>
+				</ul>
+				<p>
+					Additionally, when Panorama analyses a repository, its entire Git file structure and metadata is saved to one of our analysis servers.
+					Though it is not stored in a database, it is cached locally and may be retained for a period of time for performance reasons.
+				</p>
+				<h2>Storage of information</h2>
+				<p>
+					Your information is securely stored in our cloud servers, and it may be distributed and utilised across several of our servers and databases.
+				</p>
+				<h3>Data retention</h3>
+				<p>
+					We have a rigorous data eviction policy, which means that personal information and other data stored will be permanently and irreversibly deleted
+					from our systems once it has been deemed unnecessary.<br><br>
+					If a repository has not been analysed for a certain period of time (at most 30 days), its analysis data and all the personal information related to it
+					is permanently and irreversibly deleted.<br><br>
+					If a user has not used Panorama and its services for a certain period of time (at most 30 days), and their information is not related to
+					any repositories or analyses still present in the system, it will be permanently and irreversibly deleted.
+				</p>
+				<h2>Your rights</h2>
+				<p>
+					Under data protection law, you have rights including:
+				</p>
+				<ul>
+					<li>
+						<i>Right of access</i>: You have the right to ask us for copies of your personal information.
+					</li>
+					<li>
+						<i>Right to rectification</i>: You have the right to ask us to rectify personal information you think is inaccurate.
+						You also have the right to ask us to complete information you think is incomplete.
+					</li>
+					<li>
+						<i>Right to erasure</i>: You have the right to ask us to erase your personal information in certain circumstances.
+					</li>
+					<li>
+						<i>Right to restriction of processing</i>: You have the right to ask us to restrict the processing of your personal information
+						in certain circumstances.
+					</li>
+					<li>
+						<i>Right to object to processing</i>: You have the right to object to the processing of your personal information
+						in certain circumstsances.
+					</li>
+					<li>
+						<i>Right to data portability</i>: You have the right to ask that we transfer the personal information you gave us to another
+						organisation, or to you, in certain circumstances.
+					</li>
+				</ul>
+				<p>You are not required to pay any charge for exercising your rights. If you make a request, please allow for a reasonable amount of time for us to respond.</p>
+				<h2>Contact us</h2>
+				<p>To make an inquiry, please contact us at <b>francesco.compagnoni@kcl.ac.uk</b>.</p>
+				<br>
+				<content-footer/>
 			</div>
-			<h1>Privacy Policy</h1>
-			<i>Last updated March 25<sup>th</sup> 2021.</i>
-			<p>
-				<b>Panorama</b> ("Panorama", "we", or "us") is committed to protecting your privacy.
-				This Privacy policy explains the methods and reasons we collect, use, disclose, transfer and store your information.
-				If you have any questions about the contents of this policy, please contact us.
-			</p>
-			<h2>Information collected</h2>
-			<h3>Personal information</h3>
-			<p>
-				We collect and store your personal information when you choose to sign in to our service using GitHub.
-				The scope of the information collected is as follows:
-			</p>
-			<ul>
-				<li>Your GitHub account's unique identifier;</li>
-				<li>Your GitHub username;</li>
-				<li>The last time you used Panorama to analyse a repository;</li>
-				<li>Metadata about your GitHub profile picture.</li>
-			</ul>
-			<p>
-				It's important to note that the personal information of people who either own or have contributed to GitHub repositories that you have analysed using Panorama will also be collected in the same way.<br>
-				Additionally, during analysis, Panorama will temporarily process the email addresses and Git commit information of all repository contributors, though this data 
-				is never specifically stored or saved.
-			</p>
-			<h3>Information about repositories</h3>
-			<p>
-				Panorama will store metadata about the repositories that you have analysed using our service. 
-				This includes the following information:
-			</p>
-			<ul>
-				<li>The GitHub repository's unique identifier;</li>
-				<li>The name of the repository;</li>
-				<li>The last time the repository was analysed;</li>
-				<li>Analysis metadata and code analysis metrics tied to each contributor.</li>
-			</ul>
-			<p>
-				Additionally, when Panorama analyses a repository, its entire Git file structure and metadata is saved to one of our analysis servers.
-				Though it is not stored in a database, it is cached locally and may be retained for a period of time for performance reasons.
-			</p>
-			<h2>Storage of information</h2>
-			<p>
-				Your information is securely stored in our cloud servers, and it may be distributed and utilised across several of our servers and databases.
-			</p>
-			<h3>Data retention</h3>
-			<p>
-				We have a rigorous data eviction policy, which means that personal information and other data stored will be permanently and irreversibly deleted
-				from our systems once it has been deemed unnecessary.<br><br>
-				If a repository has not been analysed for a certain period of time (at most 30 days), its analysis data and all the personal information related to it
-				is permanently and irreversibly deleted.<br><br>
-				If a user has not used Panorama and its services for a certain period of time (at most 30 days), and their information is not related to
-				any repositories or analyses still present in the system, it will be permanently and irreversibly deleted.
-			</p>
-			<h2>Your rights</h2>
-			<p>
-				Under data protection law, you have rights including:
-			</p>
-			<ul>
-				<li>
-					<i>Right of access</i>: You have the right to ask us for copies of your personal information.
-				</li>
-				<li>
-					<i>Right to rectification</i>: You have the right to ask us to rectify personal information you think is inaccurate.
-					You also have the right to ask us to complete information you think is incomplete.
-				</li>
-				<li>
-					<i>Right to erasure</i>: You have the right to ask us to erase your personal information in certain circumstances.
-				</li>
-				<li>
-					<i>Right to restriction of processing</i>: You have the right to ask us to restrict the processing of your personal information
-					in certain circumstances.
-				</li>
-				<li>
-					<i>Right to object to processing</i>: You have the right to object to the processing of your personal information
-					in certain circumstsances.
-				</li>
-				<li>
-					<i>Right to data portability</i>: You have the right to ask that we transfer the personal information you gave us to another
-					organisation, or to you, in certain circumstances.
-				</li>
-			</ul>
-			<p>You are not required to pay any charge for exercising your rights. If you make a request, please allow for a reasonable amount of time for us to respond.</p>
-			<h2>Contact us</h2>
-			<p>To make an inquiry, please contact us at <b>francesco.compagnoni@kcl.ac.uk</b>.</p>
-			<br>
-			<content-footer/>
 		</div>
 	</div>
 </template>
@@ -138,8 +140,18 @@ export default Vue.extend({
 </style>
 <style lang="scss" scoped>
 @import "../stylesheets/globals.scss";
-.page {
+.app {
+	width: 100vw;
+	height: 100vh;
 	transition: background-color 1s, color 1s;
+
+	.background {
+		width: 100vw;
+		height: 100vh;
+		
+		background-size: cover;
+		background-position: center;
+	}
 
 	&.dark {
 		background-color: #040a1c !important;
